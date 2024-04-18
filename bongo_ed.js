da7612b0ef06b43c6647079b1fc8ac146b29f523
Earle Castledine <earlec@21csw.com>
2024-04-18 15:55:34 +0800

    color-ish

--- bongo_ed.js
+++ bongo_ed.js
@@ -14,19 +14,19 @@
         return out;
     };
 
-    const flip_v = (tile) => tile.toReversed();
-    const flip_h = (tile) => {
+    const b = (byte) => byte.toString(2).padStart(8, "0");
+    const rot90 = (tile) => {
         const out = [];
-        for (let col = 7; col >= 0; col--) {
+        for (let col = 0; col < 8; col++) {
+            const mask = 1 << (7 - col); // 128, 64, 32, 16, 8, 4, 2, 1
+            const flp = [1, 2, 4, 8, 16, 32, 64, 128];
             let v = 0;
-            const mask = 1 << col; // 128, 64, 32, 16, 8, 4, 2, 1;
             for (let row = 0; row < 8; row++) {
-                const shr = 7 - row; // 7,6,5,4,3,2,1,0
-                v |= (tile[row] & mask) >> shr;
+                const shr = row; // 0,1,2,3,4,5,6,7
+                v += !!(tile[row] & mask) << row;
             }
             out.push(v);
         }
-        //console.log(tile, out);
         return out;
     };
 
@@ -36,22 +36,20 @@
     const pix = ctx.getImageData(0, 0, w, h);
 
     const bytes1 = await getRomBytes("b-h.bin");
-    const bytes2 = await getRomBytes("b-h.bin");
-    const tiles1 = chunk(bytes1, 8).map(flip_v); //.map(flip_h);
-    const tiles2 = chunk(bytes2, 8).map(flip_v); //.map(flip_h);
+    const bytes2 = await getRomBytes("b-k.bin");
+    const tiles1 = chunk(bytes1, 8);
+    const tiles2 = chunk(bytes2, 8);
 
-    let tiles = tiles1;
-
-    const drawTile = (tile, x, y) => {
+    const drawTile = (tile, x, y, r = 0, g = 0, b = 0) => {
         for (let j = 0; j < 8; j++) {
-            const byte = tile[j];
+            const row = tile[j];
             for (let i = 0; i < 8; i++) {
-                if (byte & (1 << i)) {
+                if (row & (1 << (7 - i))) {
                     const off = (y * w + j * w + x + i) * 4;
-                    pix.data[off] = 0;
-                    pix.data[off + 1] = 1;
-                    pix.data[off + 2] = 2;
-                    pix.data[off + 3] = 255;
+                    pix.data[off + 0] += r;
+                    pix.data[off + 1] += g;
+                    pix.data[off + 2] += b;
+                    pix.data[off + 3] += 100;
                 }
             }
         }
@@ -59,19 +57,17 @@
 
     const tw = 32;
     const th = 16;
-    drawTile(tiles1[4], 0, 0);
-    drawTile(flip_h(tiles1[4]), 0, 8);
-    /*
+    const t = tiles1[1 * tw + 15];
+    const t2 = tiles2[1 * tw + 15];
+    drawTile(t, 8, 16);
+    drawTile(t2, 17, 16);
+
     for (let j = 0; j < th; j++) {
         for (let i = 0; i < tw; i++) {
-            drawTile(j * tw + i, i * 8, j * 8);
+            const t = j * tw + i;
+            drawTile(rot90(tiles1[t]), i * 8, j * 8, 100, 0, 0);
+            drawTile(rot90(tiles2[t]), i * 8, j * 8, 0, 100, 0);
         }
     }
-    tiles = tiles2;
-    for (let j = 0; j < th; j++) {
-        for (let i = 0; i < tw; i++) {
-            drawTile(j * tw + i, i * 8, (j + 16) * 8);
-        }
-    }*/
     ctx.putImageData(pix, 0, 0);
 })();
