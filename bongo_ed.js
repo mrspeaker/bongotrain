741e3264ee8cee6fce209ade63c1f79d9fb0ab0e
Mr Speaker <mrspeaker@gmail.com>
2024-04-18 20:28:21 +0800

    all words

--- bongo_ed.js
+++ bongo_ed.js
@@ -66,4 +66,43 @@
     }
 
     ctx.putImageData(pix, 0, 0);
+
+    const fetches = [1, 2, 3, 4, 5, 6].map((v) => getRomBytes(`bg${v}.bin`));
+    const get_words = (bytes) => {
+        const all = bytes.reduce(
+            (ac, el) => {
+                const isAlpha = el >= 16 && el < 16 + 26;
+                if (ac.inWord) {
+                    if (!isAlpha) {
+                        ac.words.push([...ac.word]);
+                        ac.word.length = 0;
+                        ac.inWord = false;
+                        return ac;
+                    } else {
+                        ac.word.push(el);
+                        return ac;
+                    }
+                } else if (isAlpha) {
+                    ac.inWord = true;
+                    ac.word.push(el);
+                }
+                return ac;
+            },
+            { inWord: false, word: [], words: [] },
+        ).words;
+
+        return all
+            .filter((w) => w.length > 2)
+            .map((w) =>
+                w.map((v) =>
+                    v === 16 ? " " : String.fromCharCode(v - 16 + 64),
+                ),
+            )
+            .flat()
+            .join("");
+    };
+
+    Promise.all(fetches)
+        .then((b) => b.map(get_words))
+        .then(console.log);
 })();
